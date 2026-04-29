4b08f71601e81e1cbba763b588b8386ccb34fd7a
Rei <vladbondarovich@gmail.com>
2026-04-29 23:57:46 +0300

    Update content.js v2.0.2

--- content.js
+++ content.js
@@ -1,26 +1,15 @@
-(function() {
-    function loadSettingsAndInject() {
-        chrome.storage.local.get('fbcs_settings', (result) => {
-            const settings = result.fbcs_settings || {};
-            const script = document.createElement('script');
-            script.src = chrome.runtime.getURL('inject.js');
-            script.onload = () => {
-                window.postMessage({ type: 'FBCS_SETTINGS', settings }, '*');
-                script.remove();
-            };
-            (document.head || document.documentElement).appendChild(script);
-        });
+(() => {
+    function sendSettings(settings) {
+        window.postMessage({ type: 'FBCS_SETTINGS', settings }, '*');
     }
+
+    chrome.storage.local.get('fbcs_settings', (result) => {
+        sendSettings(result.fbcs_settings || {});
+    });
 
     chrome.runtime.onMessage.addListener((msg) => {
         if (msg.type === 'UPDATE_SETTINGS') {
-            window.postMessage({ type: 'FBCS_SETTINGS', settings: msg.settings }, '*');
+            sendSettings(msg.settings);
         }
     });
-
-    if (document.readyState === 'loading') {
-        document.addEventListener('DOMContentLoaded', loadSettingsAndInject);
-    } else {
-        loadSettingsAndInject();
-    }
 })();
